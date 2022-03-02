6bfe1b9fd930250ba27d21f2cae9bdfcfc31f0bf
Decilap denis <decilapdenis@gmail.com>
2022-03-02 02:08:34 +0100

    test 10 deploy

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
